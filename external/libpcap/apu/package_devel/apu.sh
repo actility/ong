58166e1afd1c041e933f5f2a9fddbfca74bceffb
Jean-Baptiste REICH <jean-baptiste.reich@actility.com>
2014-11-20 17:34:14 +0100

    First integration of ntc6200

--- external/libpcap/apu/package_devel/apu.sh
+++ external/libpcap/apu/package_devel/apu.sh
@@ -1,48 +1,12 @@
 
 buildProject()
 {
-  ROOTACT=`pwd`
-  FLAGS=
-  case "$1" in 
-    centos6-x86)
-      CROSS_COMPILATION=0
-      ;;
-    cov1)
-      CROSS_COMPILATION=1
-      FLAGS=-mcpu=arm920t
-      PREFIX=arm-linux
-      ;;
-    cov2)
-      CROSS_COMPILATION=1
-      FLAGS=-mcpu=arm920t
-      PREFIX=arm-none-linux-gnueabi
-      ;;
-    lpv3)
-      CROSS_COMPILATION=1
-      PREFIX=mips-openwrt-linux-uclibc
-      ;;
-    rpib)
-      CROSS_COMPILATION=1
-      source $RPI_B_HOST_HOME/../../environment-setup-armv6-vfp-poky-linux-gnueabi
-      PREFIX=arm-poky-linux-gnueabi
-      ;;
-    *)
-      ;;
-  esac
-
   cd libpcap
   tar zxvf libpcap-1.3.0.tar.gz
   cd libpcap-1.3.0/
   if [ ! -f Makefile ]; then
-    if [ $CROSS_COMPILATION = 1 ]; then
-        if [ "x" != "x$FLAGS" ]; then
-            CFLAGS="$FLAGS" LDFLAGS="$FLAGS" ./configure --with-pcap=linux --host $PREFIX --prefix $ROOTACT
-        else
-            ./configure --with-pcap=linux --host $PREFIX --prefix $ROOTACT
-        fi
-    else
-        ./configure --with-pcap=linux --prefix $ROOTACT
-    fi
+    echo ./configure --with-pcap=linux $CONFIGURE_FLAGS --prefix $ROOTACT
+    ./configure --with-pcap=linux $CONFIGURE_FLAGS --prefix $ROOTACT
   fi
   make
   make install
@@ -56,7 +20,7 @@ buildApuImage()
   mkdir -p apu/data/
   mkdir -p apu/control
  
-  PCAP_VERSION=`projectVersion`
+  PCAP_VERSION=$(projectVersion)
 
   cp -r include apu/data/
   cp -r lib apu/data/ 
