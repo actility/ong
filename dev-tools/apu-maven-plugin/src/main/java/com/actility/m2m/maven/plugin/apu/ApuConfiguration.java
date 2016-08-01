9abf9010bdbbe22066486aa8eb0188ab5a350f21
Jean-Baptiste REICH <jean-baptiste.reich@actility.com>
2016-08-01 15:42:54 +0200

    Cleanup headers

--- dev-tools/apu-maven-plugin/src/main/java/com/actility/m2m/maven/plugin/apu/ApuConfiguration.java
+++ dev-tools/apu-maven-plugin/src/main/java/com/actility/m2m/maven/plugin/apu/ApuConfiguration.java
@@ -1,33 +1,30 @@
+/*******************************************************************************
+ * Copyright   Actility, SA. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 only, as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details (a copy is
+ * included at /legal/license.txt).
+ * 
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this work; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ * 
+ * Please contact Actility, SA.,  4, rue Ampere 22300 LANNION FRANCE
+ * or visit www.actility.com if you need additional
+ * information or have any questions.
+ *******************************************************************************/
 package com.actility.m2m.maven.plugin.apu;
-
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 
 import java.io.File;
 
-/**
- * Capture common archive configuration.
- *
- * @author <a href="mailto:brett@apache.org">Brett Porter</a>
- * @version $Id: MavenArchiveConfiguration.java 1233956 2012-01-20 15:27:43Z olamy $
- * @todo is this general enough to be in Plexus Archiver?
- */
 public class ApuConfiguration {
 
     private File controlFile;
--- dev-tools/apu-maven-plugin/src/main/java/com/actility/m2m/maven/plugin/apu/Control.java
+++ dev-tools/apu-maven-plugin/src/main/java/com/actility/m2m/maven/plugin/apu/Control.java
@@ -1,24 +1,30 @@
+/*******************************************************************************
+ * Copyright   Actility, SA. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 only, as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details (a copy is
+ * included at /legal/license.txt).
+ * 
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this work; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ * 
+ * Please contact Actility, SA.,  4, rue Ampere 22300 LANNION FRANCE
+ * or visit www.actility.com if you need additional
+ * information or have any questions.
+ *******************************************************************************/
 package com.actility.m2m.maven.plugin.apu;
 
 import java.io.IOException;
 import java.io.Writer;
-
-/**
- *
- * Copyright 2004 The Apache Software Foundation
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 
 
 /**
